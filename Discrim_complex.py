e0ead9378b7c02b1feab65ccd50538b1f6793d4a
Александр Юрлов <alexandryurlov@gmail.com>
2022-12-04 21:23:42 +0300

    first commit

---
+++ Discrim_complex.py
@@ -0,0 +1,26 @@
+import cmath
+ 
+print("Введите коэф.для квадратного уравнени")
+print("ax^2 + bx + c = 0:")
+a=complex(input("a = "))
+b=complex(input("b = "))
+c=complex(input("c = "))
+        
+if a==0 and b==0 and c==0:
+    print("X-принимает любое число")
+elif a==0 and b==0:
+    print ("решений нет")
+elif a==0 and c==0:
+    print("x=",0)
+elif a==0:
+    x=(-c/b)
+    print("x=",x)
+elif b==0:
+    print("x=", cmath.sqrt(-c/a))
+else:
+    D = b ** 2 - 4 * a * c
+    print(f"Дискриминант = {D}")
+    x1=(-b+cmath.sqrt(D))/(2*a)
+    x2=(-b-cmath.sqrt(D))/(2*a)
+    print("x1=",x1)
+    print("x2=",x2)
---
+++ Discrimenant.py
@@ -0,0 +1,30 @@
+import math
+ 
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+if a==0 and b==0 and c==0:
+    print("X-принимает любое число")
+elif a==0 and b==0:
+    print ("решений нет")
+elif a==0 and c==0:
+    print("x=",0)
+elif a==0:
+    x=(-c/b)
+    print("x=",x)
+else:
+    D = b ** 2 - 4 * a * c
+    print(f"Дискриминант = {D}")
+ 
+    if D > 0:
+        x1 = (-b + math.sqrt(D)) / (2 * a)
+        x2 = (-b - math.sqrt(D)) / (2 * a)
+        print(f"x1 = {x1} \nx2 = {x2}")
+    elif D == 0:
+        x = -b / (2 * a)
+        print(f"x = {x}")
+    else:
+        print("Корней нет")
